064a86bd748280fb54a8be91eb2111a0042df1da
weru <147871343+marshaweru@users.noreply.github.com>
2023-11-09 00:50:05 -0800

    Copy paste for the other 3 questions

--- js/main.js
+++ js/main.js
@@ -3,7 +3,17 @@ const questions = {
     {
         question: "What's the fastest aircraft in the world?",
         answers: [
-            { text: "777", correct: fasle}
+            { text: "777", correct: fasle},
+            { text: "SR-17", correct: true},
+            { text: "A380", correct: false},
+            { text: "F-16", correct: false},
         ]
-    }
+    },
 }
+        question: "What's the fastest aircraft in the world?",
+        answers: [
+            { text: "777", correct: fasle},
+            { text: "SR-17", correct: true},
+            { text: "A380", correct: false},
+            { text: "F-16", correct: false},
+        ]
